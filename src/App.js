8460fc7951b8b59413986d4d218d0b5c1cdee949
Paolo Salvatori <leprino@hotmail.com>
2022-02-03 14:07:49 +0100

    Added API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+/*
 import React from 'react';
 
 function App() {
   const value = 'World';
   return <div>Hello {value}</div>;
+}
+
+export default App;
+*/
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
